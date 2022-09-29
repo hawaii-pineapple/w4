17007aaf0393073824df86c4dbd08403d24b395e
hawaii <113569076+hawaii-pineapple@users.noreply.github.com>
2022-09-29 11:59:41 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -10,7 +10,7 @@ function randomValueFromArray(array){
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['教授', '友達', '幽霊'];
 const insertY = ['PayPayドーム', '渋谷', '津田塾大学'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const insertZ = ['たぬきが現れた', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
