7cc1eb304b8c1e376447ff673fd97895424ecf5b
hawaii <113569076+hawaii-pineapple@users.noreply.github.com>
2022-09-29 11:13:20 +0900

    Update main.js

--- w3/main.js
+++ w3/main.js
@@ -13,7 +13,8 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
+  //let myName = prompt('Please enter your name.');
+  let myName = "seika";
   if(!myName) {
     setUserName();
   } else {
